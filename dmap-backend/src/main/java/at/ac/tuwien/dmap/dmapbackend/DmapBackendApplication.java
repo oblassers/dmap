497df0ee27fbb3e1de367522c5665b9d39db7d08
Simon Oblasser <simon.oblasser@student.tuwien.ac.at>
2018-12-04 18:33:52 +0100

    setup basic project structure with vue.js frontend + spring boot backend

---
+++ dmap-backend/src/main/java/at/ac/tuwien/dmap/dmapbackend/DmapBackendApplication.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.dmap.dmapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmapBackendApplication.class, args);
+	}
+}
---
+++ dmap-backend/src/test/java/at/ac/tuwien/dmap/dmapbackend/controller/BackendControllerTest.java
@@ -0,0 +1,42 @@
+package at.ac.tuwien.dmap.dmapbackend.controller;
+
+import at.ac.tuwien.dmap.dmapbackend.DmapBackendApplication;
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        classes = DmapBackendApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+public class BackendControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Before
+    public void init() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void saysHello() {
+        when()
+                .get("/api/hello")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .assertThat()
+                .body(is(equalTo(BackendController.HELLO_TEXT)));
+    }
+}
